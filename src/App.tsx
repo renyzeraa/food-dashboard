9be8681ac30c65317a92c9cb23d887f8c47c674b
Renan Silva <renansilvaytB@gmail.com>
2025-12-16 18:24:46 -0300

    feat: initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router'
+import { router } from './routes'
 
 export function App() {
   return (
-    <>
-      <h1 className="flex items-center text-2xl">Hello World</h1>
-      <Button type="button">Clicar aqui</Button>
-    </>
+    <RouterProvider router={router} />
   )
 }
