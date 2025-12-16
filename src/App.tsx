2780bea8681dff117f752ef0be2f8e9b7113acc5
Renan Silva <renansilvaytB@gmail.com>
2025-12-16 18:13:52 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <>
+      <h1 className="flex items-center text-2xl">Hello World</h1>
+      <Button type="button">Clicar aqui</Button>
+    </>
+  )
+}
