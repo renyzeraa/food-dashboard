395bc7619fbeb0a7e25a7197230db7e5258be817
Renan Silva <renan.leandro.t_db@grupoboticario.com.br>
2026-01-28 20:32:11 -0300

    feat: pedidos para periodo de 7 dias

---
+++ src/components/date-range-picker.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import { Field, FieldLabel } from "@/components/ui/field"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { type DateRange } from "react-day-picker"
+
+interface DatePickerRangeProps {
+  label: string
+  date: DateRange | undefined
+  onDateChange: (date: DateRange | undefined) => void
+}
+
+export function DatePickerRange({ label, date, onDateChange }: DatePickerRangeProps) {
+
+  return (
+    <Field className="mx-auto w-60">
+      <FieldLabel htmlFor="date-picker-range">{label}</FieldLabel>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date-picker-range"
+            className="justify-start px-2.5 font-normal"
+          >
+            <CalendarIcon />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={onDateChange}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </Field>
+  )
+}
--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,19 +1,35 @@
+import { getDailyRevenueInPeriod } from "@/api/get-daily-revenue-in-period";
+import { DatePickerRange } from "@/components/date-range-picker";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatToBRL } from "@/utils/formater";
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useMemo, useState } from "react";
+import type { DateRange } from "react-day-picker";
 import { ResponsiveContainer, LineChart, YAxis, Line, XAxis, CartesianGrid } from 'recharts';
 import colors from 'tailwindcss/colors';
 
-const data = [
-    { date: '10/01', revenue: 1200 },
-    { date: '11/02', revenue: 1100 },
-    { date: '12/02', revenue: 300 },
-    { date: '13/02', revenue: 400 },
-    { date: '14/02', revenue: 700 },
-    { date: '15/02', revenue: 600 },
-    { date: '16/02', revenue: 1500 },
-]
-
 export function RevenueChart() {
+    const [dateRange, setDateRange] = useState<DateRange>({
+        from: subDays(new Date(), 7),
+        to: new Date()
+    })
+
+    const { data: dailyRevenueInPeriod } = useQuery({
+        queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+        queryFn: () => getDailyRevenueInPeriod({
+            from: dateRange?.from,
+            to: dateRange?.to
+        })
+    })
+
+    const chartData = useMemo(() => {
+        return dailyRevenueInPeriod?.map(item => ({
+            ...item,
+            receipt: item.receipt / 100
+        }))
+    }, [dailyRevenueInPeriod]);
+
     return (
         <Card className="col-span-6">
             <CardHeader className="flex! items-center justify-between pb-8">
@@ -25,31 +41,41 @@ export function RevenueChart() {
                         Receita diária no período
                     </CardDescription>
                 </div>
+
+                <div className="flex items-center gap-3">
+                    <DatePickerRange
+                        label="Período"
+                        date={dateRange}
+                        onDateChange={(date) => date && setDateRange(date)}
+                    />
+                </div>
             </CardHeader>
 
             <CardContent>
-                <ResponsiveContainer width="100%" height={240}>
-                    <LineChart
-                        data={data}
-                        style={{ fontSize: 12 }}
-                    >
-                        <XAxis
-                            dataKey="date"
-                            axisLine={false}
-                            tickLine={false}
-                            dy={16}
-                        />
-                        <YAxis
-                            stroke="#888"
-                            axisLine={false}
-                            tickLine={false}
-                            width={80}
-                            tickFormatter={value => formatToBRL(value as number)}
-                        />
-                        <CartesianGrid vertical={false} className="stroke-muted" />
-                        <Line type="linear" dataKey="revenue" strokeWidth={2} stroke={colors.emerald[500]} />
-                    </LineChart>
-                </ResponsiveContainer>
+                {chartData &&
+                    <ResponsiveContainer width="100%" height={240}>
+                        <LineChart
+                            data={chartData}
+                            style={{ fontSize: 12 }}
+                        >
+                            <XAxis
+                                dataKey="date"
+                                axisLine={false}
+                                tickLine={false}
+                                dy={16}
+                            />
+                            <YAxis
+                                stroke="#888"
+                                axisLine={false}
+                                tickLine={false}
+                                width={80}
+                                tickFormatter={value => formatToBRL(value as number)}
+                            />
+                            <CartesianGrid vertical={false} className="stroke-muted" />
+                            <Line type="linear" dataKey="receipt" strokeWidth={2} stroke={colors.emerald[500]} />
+                        </LineChart>
+                    </ResponsiveContainer>
+                }
             </CardContent>
         </Card>
     )
